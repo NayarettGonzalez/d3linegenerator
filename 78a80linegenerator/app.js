afec409dc33b930da614344c703a8957b8fc0f5a
NayarettGonzalez <nagonzalezv@uc.cl>
2021-02-16 17:58:55 -0300

    Se definen distintas entradas de la data

--- 78a80linegenerator/app.js
+++ 78a80linegenerator/app.js
@@ -12,15 +12,31 @@ let points = [
     [100, 300]
 ];
 
+//SE ESPECIFICAN COORDENADAS DE LOS PUNTOS
+let data = [
+    { x: 100, y: 100 }, { x: 130, y: 260 }, { x: 300, y: 160 }, { x: 500, y: 250 }
+    // { point: 100 }, { point: 150 }, { point: 80 }, { point: 220 }, { point: 130 }
+];
 let width = 600,
     height = 500;
 
 //SE ESPECIFICA LAS CARACTERÍSTICAS DEL SVG QUE CONTENDRA EL GRÁFICO
 let svg = d3.select("body").append("svg")
-    .attr("width", width).attr("height", height)
-    .style("background", "pink");
+    .attr("width", width).attr("height", height);
 
 //SE DEFINE EL GENERADOR DE LINEAS DE D3
-let line = d3.line();
+// let line = d3.line();
+// svg.append("path").attr("d", line(points));
 
-svg.append("path").attr("d", line(points));
+//SE DEFINE LÍNEA PARA PUNTOS DADAS LAS COORDENADAS
+let line = d3.line()
+    .x((d) => d.x)
+    .y((d) => d.y);
+
+//SE DEFINE UNA LÍNEA CURVA
+// let line = d3.line()
+//     .x((d) => d.point)
+//     .y((d, i) => (i + 1) * 50)
+//     .curve(d3.curveBundle);
+
+svg.append("path").attr("d", line(data));
